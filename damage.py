2ada639a38b42a3be574821459013d11ccd84241
PaavoNiskanen <Paavo.Niskanen@seamk.fi>
2026-02-04 13:49:51 +0200

    damage-aloitus

---
+++ damage.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Damage(pygame.sprite.sprite): 
+    def __init__():
+        
+
+    def dealing_damage_to_enemy():
+        self.enemy_lives = 1
+
+        if player_laser = enemy:
+            self.enemy_lives -= 1
+
+    def taking_damge_from_enemy():
+        self.lives = 3 
+
+        if enemy_laser = player:
+            self.lives -= 1
+
+    def collision_from_meteor():
+        if meteor = player:
+            self.live -=1 
+            # ei ota damagea meteoriitista 3 sekunttiin
+
